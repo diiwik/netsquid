5c02e22d0752f1e9c6b8c56bbd409f46e0b5b1ef
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-08-21 12:24:39 +0000

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='UIIIT-NetSquid',
+    version='0.1',
+    packages=['uiiit'],
+    package_dir={'': './'},
+    url='https://github.com/ccicconetti/netsquid/',
+    license='MIT',
+    keywords='quantum network benchmarking',
+    author='Claudio Cicconetti',
+    entry_points={},
+)
