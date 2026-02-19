a2382a26c5468354270254d9a591b1db116a406b
diiwik <23ucc538@lnmiit.ac.in>
2026-02-19 09:11:20 +0530

    Update create-qubits.py

--- Tutorial/create-qubits.py
+++ Tutorial/create-qubits.py
@@ -12,11 +12,13 @@ ns.qubits.operate([qubits[0], qubits[1]], ns.CX)
 
 # the first bit can be 0 or 1 in the computational basis
 res0 = ns.qubits.measure(qubits[0], observable=ns.Z)
+#measure returns result and prob
 assert 0.49 <= res0[1] <= 0.51
 
 # the second qubit must the same as the first one
 res1 = ns.qubits.measure(qubits[1], observable=ns.Z)
 assert res0[0] == res1[0]
+#next line tells us that the prob of res1[1] is 1
 assert res1[1] == 1
 
 # if we measure again we find the same results as before, with certainty
